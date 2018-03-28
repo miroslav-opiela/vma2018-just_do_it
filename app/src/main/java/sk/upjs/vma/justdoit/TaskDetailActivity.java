c2d616ee2523c8eb5590a42b6f32c03f34c7844d
Miroslav Opiela <miroslav.opiela@student.upjs.sk>
2018-03-28 21:41:01 +0200

    Odstranovanie ulohy

--- app/src/main/java/sk/upjs/vma/justdoit/TaskDetailActivity.java
+++ app/src/main/java/sk/upjs/vma/justdoit/TaskDetailActivity.java
@@ -3,6 +3,8 @@ package sk.upjs.vma.justdoit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
@@ -15,6 +17,8 @@ public class TaskDetailActivity extends AppCompatActivity {
 
     private EditText editTextTaskName;
     private CheckBox checkBoxTaskCompleted;
+
+    private boolean ignoreSaveOnFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +52,35 @@ public class TaskDetailActivity extends AppCompatActivity {
     }
 
     private void saveTask() {
+        // v pripade ze sa odstranuje uloha a nema byt ulozena
+        if (ignoreSaveOnFinish) {
+            ignoreSaveOnFinish = false;
+            return;
+        }
+
         task.setName(editTextTaskName.getText().toString());
         task.setDone(checkBoxTaskCompleted.isChecked());
         taskDao.saveOrUpdate(task);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.task_detail_action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_item_delete) {
+            taskDao.delete(task);
+            // priznak aby sa uloha neukladala po navrate z detail aktivity
+            ignoreSaveOnFinish = true;
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
